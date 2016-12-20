b278a41bf54bf88fe13b55b908ee1038f85cfca4
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2016-12-20 14:51:08 +0100

    updated update.sh script

--- Util/update.sh
+++ Util/update.sh
@@ -8,9 +8,9 @@ rm -rf ArduinoTemp
 
 echo Pulling latest QRoboticsCenter version
 git clone https://github.com/maartenverbandt/QRoboticsCenter.git QRoboticsCenterTemp
-cp -rf QRoboticsCenterTemp/build Desktop/QRoboticsCenter
-cp -rf QRoboticsCenterTemp/Matlab Desktop/Matlab
-cp -rf QRoboticsCenterTemp/python Desktop/python
+cp -rf QRoboticsCenterTemp/build ./Desktop/QRoboticsCenter
+cp -rf QRoboticsCenterTemp/Matlab ./Desktop/Matlab
+cp -rf QRoboticsCenterTemp/python ./Desktop/Python
 cp -rf QRoboticsCenterTemp/icons/QRoboticsCenterLogo_blue.png Desktop/QRoboticsCenter.png
 cp -rf QRoboticsCenterTemp/README.md Desktop/readme.md
 rm -rf QRoboticsCenterTemp
