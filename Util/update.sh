ff976cb03d221d74d42ed62778302e93f8ad229d
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-09-28 18:17:43 +0200

    Update MECO-CSI from parent repos

--- Util/update.sh
+++ Util/update.sh
@@ -9,8 +9,8 @@ rm -rf ArduinoTemp
 echo Pulling latest QRoboticsCenter version
 git clone https://github.com/maartenverbandt/QRoboticsCenter.git QRoboticsCenterTemp
 cp -rf QRoboticsCenterTemp/build ./Desktop/QRoboticsCenter
-cp -rf QRoboticsCenterTemp/Matlab ./Desktop/Matlab
-cp -rf QRoboticsCenterTemp/python ./Desktop/Python
-cp -rf QRoboticsCenterTemp/icons/QRoboticsCenterLogo_blue.png Desktop/QRoboticsCenter.png
-cp -rf QRoboticsCenterTemp/README.md Desktop/readme.md
+cp -rf QRoboticsCenterTemp/Matlab ./Desktop
+cp -rf QRoboticsCenterTemp/python ./Desktop
+cp -rf QRoboticsCenterTemp/icons/QRoboticsCenterLogo_blue.png ./Desktop/QRoboticsCenter.png
+cp -rf QRoboticsCenterTemp/readme.md ./Desktop/readme.md
 rm -rf QRoboticsCenterTemp
